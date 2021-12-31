4d7a9ddd1dadbbab651aa9909834cc7797a92edd
Abhi Wadekar <abhi1@berkeley.com>
2021-12-31 01:25:40 -0800

    added clicker [beta]

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const handleChange = () => {
+    setCount(count+1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app, and it works!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Count: {count}</Text>
+      <Button title='change count' onPress={handleChange}/>
       <StatusBar style="auto" />
     </View>
   );
