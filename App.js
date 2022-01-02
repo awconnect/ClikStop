55338246004b2d6ccf243ed6558bb59fd7784a45
ShreyasNarayanan18 <snaray52@asu.edu>
2022-01-02 01:51:52 -0800

    it actually fucking workssssss

--- App.js
+++ App.js
@@ -2,22 +2,45 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+function StopWatch() {
   const [started, setStarted] = useState(false);
-  const [time, setTime] = useState(0);
-  const [startTime, setStartTime] = useState(0);
+  const [isFirst, setIsFirst] = useState(false);
+  let defTime = new Date();
+  const [time, setTime] = useState(new Date(defTime - defTime));
+  const [startTime, setStartTime] = useState(null);
+  const [topTime, setTopTime] = useState(null);
 
+
+  function getTimeIncrements(date) {
+    var hour = date.getUTCHours()
+      , min = date.getUTCMinutes()
+      , sec = date.getUTCSeconds()
+      , ms = date.getUTCMilliseconds();
+     return ((hour > 9 ? hour : "0" + hour) + ":" + 
+      (min > 9 ? min : "0" + min) + ":" + 
+      (sec > 9 ? sec : "0" + sec) + "." + 
+      (ms > 99 ? ms : ms > 9 ? "0" + ms : "00" + ms));
+  }
 
   useEffect(() => {
     let interval = null;
     if (started) {
       interval = setInterval(() => {
-        if (startTime === 0) {
-          setStartTime(Date.now());
+        if (startTime === null) {
+          setStartTime(new Date());
         }
-        setTime(Date.now() - startTime);
+        let currentTime = new Date();
+        setTime(new Date(currentTime - startTime));
       }, 10);
-    } else if (!started && time !== 0) {
+    } else if (!started && isFirst) {
+      if (topTime === null) {
+        setTopTime(time);
+      }
+      else {
+        if (topTime > time) {
+           setTopTime(time);
+        } 
+      }
       clearInterval(interval);
     }
     return () => {
@@ -26,14 +49,23 @@ export default function App() {
   }, [started, time]);
 
   const reset = () => {
-    setTime(0);
+    let defTime = new Date()
+    setTime(new Date(defTime - defTime));
     setStarted(false);
-    setStartTime(0);
+    setStartTime(null);
+    setIsFirst(false);
   }
 
   const isStarted = () => {
+    if (started === true) {
+      setStartTime(null);
+    }
     setStarted(!started);
+    if (!isFirst) {
+      setIsFirst(true);
+    }
   }
+
 
   function convToFullTime(millis) {
     var minutes = Math.floor(millis / 60000);
@@ -42,14 +74,28 @@ export default function App() {
     return minutes + ":" + (seconds < 10 ? '0' : '') + seconds + ":" + ms;
   }
 
+  if(topTime === null) {
+    return (
+      <View style={styles.container}>
+        <View>
+          <Text>{getTimeIncrements(time)}</Text>
+        </View>
+
+        <Button title='Start' onPress={isStarted}/>
+        <Button title='Reset' onPress={reset}/>
+      </View>
+    );
+  }
   if (!started) {
     return (
       <View style={styles.container}>
         <View>
-          <Text>{convToFullTime(time)}s</Text>
+          <Text>{getTimeIncrements(time)}</Text>
+
         </View>
   
         <View>
+          <Text> Best Time: {getTimeIncrements(topTime)} </Text>
           <Button title='Start' onPress={isStarted}/>
           <Button title='Reset' onPress={reset}/>
         </View>
@@ -59,16 +105,25 @@ export default function App() {
     return (
       <View style={styles.container}>
         <View>
-          <Text>{convToFullTime(time)}s</Text>
+          <Text>{getTimeIncrements(time)}</Text>
         </View>
   
         <View>
+          <Text> Best Time: {getTimeIncrements(topTime)} </Text>
           <Button title='Stop' onPress={isStarted}/>
           <Button title='Reset' onPress={reset}/>
         </View>
       </View>
     );
   }
+}
+
+export default function App() {
+ return (
+   <View style={styles.container}>
+     <StopWatch />
+   </View>
+ );
 }
 
 const styles = StyleSheet.create({
