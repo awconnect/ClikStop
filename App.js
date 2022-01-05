b56bfea24d26f4acb7e4305bece77205e66e4a35
ShreyasNarayanan18 <snaray52@asu.edu>
2022-01-05 00:05:37 -0800

    Added useContext to app

--- App.js
+++ App.js
@@ -1,145 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState, useReducer } from 'react';
+import {StyleSheet, View } from 'react-native';
+import StopWatch  from './StopWatch';
+import Leaderboard from './Leaderboard';
 
-function StopWatch() {
-  const [started, setStarted] = useState(false);
-  const [isFirst, setIsFirst] = useState(false);
-  let defTime = new Date();
-  const [time, setTime] = useState(new Date(defTime - defTime));
-  const [startTime, setStartTime] = useState(null);
-  const [topTime, setTopTime] = useState(null);
+export const AppContext = React.createContext();
 
-
-  function getTimeIncrements(date) {
-    var hour = date.getUTCHours()
-      , min = date.getUTCMinutes()
-      , sec = date.getUTCSeconds()
-      , ms = date.getUTCMilliseconds();
-     return ((hour > 9 ? hour : "0" + hour) + ":" + 
-      (min > 9 ? min : "0" + min) + ":" + 
-      (sec > 9 ? sec : "0" + sec) + "." + 
-      (ms > 99 ? ms : ms > 9 ? "0" + ms : "00" + ms));
-  }
-
-  useEffect(() => {
-    let interval = null;
-    if (started) {
-      interval = setInterval(() => {
-        if (startTime === null) {
-          setStartTime(new Date());
-        }
-        let currentTime = new Date();
-        setTime(new Date(currentTime - startTime));
-      }, 10);
-    } else if (!started && isFirst) {
-      if (topTime === null) {
-        setTopTime(time);
-      }
-      else {
-        if (topTime > time) {
-           setTopTime(time);
-        } 
-      }
-      clearInterval(interval);
-    }
-    return () => {
-      clearInterval(interval);
-    }
-  }, [started, time]);
-
-  const reset = () => {
-    let defTime = new Date()
-    setTime(new Date(defTime - defTime));
-    setStarted(false);
-    setStartTime(null);
-    setIsFirst(false);
-  }
-
-  const isStarted = () => {
-    if (started === true) {
-      setStartTime(null);
-    }
-    setStarted(!started);
-    if (!isFirst) {
-      setIsFirst(true);
-    }
-  }
+const initialState = {
+  topTimes: [],
+};
 
 
-  function convToFullTime(millis) {
-    var minutes = Math.floor(millis / 60000);
-    var seconds = ((millis % 60000) / 1000).toFixed(0);
-    var ms = (millis % 1000);
-    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds + ":" + ms;
-  }
+export const getTimeIncrements = (date) => {
+  var hour = date.getUTCHours()
+    , min = date.getUTCMinutes()
+    , sec = date.getUTCSeconds()
+    , ms = date.getUTCMilliseconds();
+    return ((hour > 9 ? hour : "0" + hour) + ":" + 
+    (min > 9 ? min : "0" + min) + ":" + 
+    (sec > 9 ? sec : "0" + sec) + "." + 
+    (ms > 99 ? ms : ms > 9 ? "0" + ms : "00" + ms));
+}
 
-  if(topTime === null) {
-    if (!started) {
-      return (
-        <View style={styles.container}>
-          <View>
-            <Text>{getTimeIncrements(time)}</Text>
-          </View>
-  
-          <Button title='Start' onPress={isStarted}/>
-          <Button title='Reset' onPress={reset}/>
-        </View>
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          <View>
-            <Text>{getTimeIncrements(time)}</Text>
-          </View>
 
-          <Button title='Stop' onPress={isStarted}/>
-          <Button title='Reset' onPress={reset}/>
-        </View>
-      );
-    }
-  } 
-  if (!started) {
-    return (
-      <View style={styles.container}>
-        <View>
-          <Text>{getTimeIncrements(time)}</Text>
+function reducer(state, action) {
+  switch (action.type) {
+      case 'UPDATE_INPUT':
+          return {
+              topTimes: action.data
+          };
 
-        </View>
-  
-        <View>
-          <Text> Best Time: {getTimeIncrements(topTime)} </Text>
-          <Button title='Start' onPress={isStarted}/>
-          <Button title='Reset' onPress={reset}/>
-        </View>
-      </View>
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        <View>
-          <Text>{getTimeIncrements(time)}</Text>
-        </View>
-  
-        <View>
-          <Text> Best Time: {getTimeIncrements(topTime)} </Text>
-          <Button title='Stop' onPress={isStarted}/>
-          <Button title='Reset' onPress={reset}/>
-        </View>
-      </View>
-    );
+      default:
+          return initialState;
   }
 }
 
+
+
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  
+
  return (
-   <View style={styles.container}>
-     <StopWatch />
-   </View>
+  <AppContext.Provider value={{ state, dispatch }}>
+    <View style={styles.container}>
+        <StopWatch />
+        <Leaderboard />
+    </View>
+  </AppContext.Provider>
+
  );
 }
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
---
+++ Leaderboard.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import {StyleSheet, View } from 'react-native';
+import { AppContext } from './App';
+import getTimeIncrements from "./App";
+import { styles } from './App';
+
+
+
+const Leaderboard = () => {
+    const {state, dispatch} = useContext(AppContext);
+    return (
+        <View>
+            <Text>Leaderboard:</Text>
+            {
+            state.topTimes.map((topTime) => (
+                <Text key ={topTime}>{getTimeIncrements(new Date(topTime) )}</Text>
+            ))
+            }
+        </View>
+
+
+    );
+}
+ 
+export default Leaderboard;
